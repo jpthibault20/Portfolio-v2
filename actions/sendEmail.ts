e1c6341054f82e40062fbb097eaa6b5a4f88df5b
thibault <tjeanpierre757@gmail.com>
2024-01-23 18:29:22 +0100

    update env variable

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend('3c792c7d-c2e4-4905-ae07-10645955006d');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
