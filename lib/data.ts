a1d9e3be321dc84e5a63f91e8e6ccfee2e3c09e8
jpthibault20 <tjeanpierre757@gmail.com>
2024-03-12 17:46:01 +0100

    update

--- lib/data.ts
+++ lib/data.ts
@@ -116,7 +116,7 @@ export const experiencesData = [
     date: "2023",
   },
   {
-    title: "Décathlon",
+    title: "Decathlon",
     location: "Metz, fr",
     description:
       "vendeur omnicomerce (job étudiant).",
@@ -130,5 +130,13 @@ export const experiencesData = [
       "Alternance au sein de LTG-Tech (start-up) en tant qu'ingénieur puis emploi à temps complet.",
     icon: React.createElement(CgWorkAlt),
     date: "2021 - 2024",
+  },
+  {
+    title: "JP-developpement",
+    location: "Metz, fr",
+    description:
+      "",
+    icon: React.createElement(CgWorkAlt),
+    date: "2024 - now",
   },
 ] as const;
